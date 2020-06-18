d6da9d7ec1881507606f5b193a73628c76906b02
min <pakmk8@gmail.com>
2020-06-18 13:13:57 -0400

    testing mapquest search

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-$(document).ready(function () {
+// $(document).ready(function () {
  
   
-  $.ajax(settings).done(function (response) {
-    console.log(response);
+//   $.ajax(settings).done(function (response) {
+//     console.log(response);
+//   });
+// });
+
+window.onload = function() {
+  placeSearch({
+    key: 'ZZRRtGp9bjs0OUWpRQ8pC3Tgt1zc8QAR',
+    container: document.querySelector('#search-input'),
+    useDeviceLocation: true,
+    collection: [
+      'poi',
+      'airport',
+      'address',
+      'adminArea',
+    ]
   });
-});
+}
