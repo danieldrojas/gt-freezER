733774fafdcb6c660e96370d549591647eeca7c6
Daniel Rojas <daniel.drojas@outlook.com>
2020-06-19 12:52:37 -0400

    added img div

--- script.js
+++ script.js
@@ -35,17 +35,38 @@ function getIceCreamStores(loc) {
     for (var i = 0; i < 10; i++) {
       var iceCreamStores = response.businesses[i].name;
       var storeAddress = response.businesses[i].location.address1;
-      console.log(storeAddress);
-      console.log(iceCreamStores);
-      var storeList = $("<button>").text(iceCreamStores);
+      var iceCreamDistance = ("distance")
+      var timeToDistance = ("time")
+      // console.log(storeAddress);
+      // console.log(iceCreamStores);
+      var storeList = $("<button id='store-button'>").text(iceCreamStores);
+      storeList.append($("<div>" + storeAddress + "</div>"));
+      storeList.append($("<div>" + iceCreamDistance + "</div>"));
+      storeList.append($("<div>" + timeToDistance + "</div>"));
       $(storeList).attr("class", "btn-block newIceCreamStoreButton");
       var listItem = $("<li>").append(storeList);
-      $("#iceCreamStores").append(listItem);
+      $("#iceCreamStores").append(listItem)
+
+    //this add the image to the main col. In the event of click on the button
+      var image = response.businesses[i].image_url
+      console.log(image)
+      
+      var imgDiv = $("<img>")
+      imgDiv.attr('src', image)
+      $("#icecream-img").append(imgDiv)
+       
+
+ $()
+
+      
+   
+
+
     }
   });
   var mapQuestKey = "bDYO5JVsT0lGPolecMUk1lCGVNostBHT";
-  var pointA = "";
-  var pointB = "";
+  var pointA = "4645 Valais court";
+  var pointB = "Main street NW";
 
   var myURL =
     "http://www.mapquestapi.com/directions/v2/route?key=bDYO5JVsT0lGPolecMUk1lCGVNostBHT&from=" +
@@ -58,6 +79,7 @@ function getIceCreamStores(loc) {
     method: "GET",
   }).then(function (response) {
     console.log(response);
+    console.log(response)
     var distance = response.route.distance;
     var time = response.route.time;
     console.log(time);
