b624520049be861e3a7ec2ea73909bbe4459214f
Brad Williams <jb.wms89@gmail.com>
2020-06-19 11:28:42 -0400

    point B lat/lon working as intended

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+$(document).ready(function () {
+  navigator.geolocation.getCurrentPosition(
+    locationHandler,
+    locationErrorHandler,
+    options
+  );
+  $("#zipcode-submit").on("click", function () {
+    var zipcode = $("#zipcode-input").val();
+    getIceCreamStores(zipcode);
+  });
+});
+
+// pulled following location data from
+// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function locationHandler(pos) {
+  crd = pos.coords;
+  console.log(crd);
+  console.log(crd.latitude);
+  console.log(crd.longitude);
+  getIceCreamStores(crd);
+  // var latPointA = crd.latitude;
+  // var lonPointA = crd.longitude;
+  console.log("Your current position is:");
+  console.log("Latitude: " + crd.latitude);
+  console.log("Longitude: " + crd.longitude);
+}
+
+function locationErrorHandler(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
 function getIceCreamStores(loc) {
   var data = { term: "ice cream" };
   if (loc && loc.latitude) {
@@ -24,7 +62,7 @@ function getIceCreamStores(loc) {
     },
     data: data,
     success: function (result) {
-      console.log(result);
+      // console.log(result);
     },
     error: function (error) {
       console.log(error);
@@ -35,65 +73,39 @@ function getIceCreamStores(loc) {
     for (var i = 0; i < 10; i++) {
       var iceCreamStores = response.businesses[i].name;
       var storeAddress = response.businesses[i].location.address1;
-      console.log(storeAddress);
-      console.log(iceCreamStores);
+      // console.log(storeAddress);
+      // console.log(iceCreamStores);
       var storeList = $("<button>").text(iceCreamStores);
       $(storeList).attr("class", "btn-block newIceCreamStoreButton");
       var listItem = $("<li>").append(storeList);
       $("#iceCreamStores").append(listItem);
+      var latPointB = response.businesses[i].coordinates.latitude;
+      var lonPointB = response.businesses[i].coordinates.longitude;
+      
+      // locationHandler();
+      // var startingPos = latPointA + "," + lonPointA;
+      var destinationPos = latPointB + "," + lonPointB;
+      // console.log(startingPos);
+      console.log(destinationPos);
+      var mapQuestKey = "bDYO5JVsT0lGPolecMUk1lCGVNostBHT";
+      var pointA = "";
+      var pointB = destinationPos;
+
+      var myURL =
+        "http://www.mapquestapi.com/directions/v2/route?key=bDYO5JVsT0lGPolecMUk1lCGVNostBHT&from=" +
+        pointA +
+        "&to=" +
+        pointB;
+
+      $.ajax({
+        url: myURL,
+        method: "GET",
+      }).then(function (response) {
+        // console.log(response);
+        var distance = response.route.distance;
+        var time = response.route.time;
+        // console.log(time);
+      });
     }
   });
-  var mapQuestKey = "bDYO5JVsT0lGPolecMUk1lCGVNostBHT";
-  var pointA = "";
-  var pointB = "";
-
-  var myURL =
-    "http://www.mapquestapi.com/directions/v2/route?key=bDYO5JVsT0lGPolecMUk1lCGVNostBHT&from=" +
-    pointA +
-    "&to=" +
-    pointB;
-
-  $.ajax({
-    url: myURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    var distance = response.route.distance;
-    var time = response.route.time;
-    console.log(time);
-  });
-}
-
-$(document).ready(function () {
-  navigator.geolocation.getCurrentPosition(
-    locationHandler,
-    locationErrorHandler,
-    options
-  );
-  $("#zipcode-submit").on("click", function () {
-    var zipcode = $("#zipcode-input").val();
-    getIceCreamStores(zipcode);
-  });
-});
-
-// pulled following location data from
-// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
-
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
-
-function locationHandler(pos) {
-  var crd = pos.coords;
-  getIceCreamStores(crd);
-  console.log("Your current position is:");
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
-}
-
-function locationErrorHandler(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
 }
