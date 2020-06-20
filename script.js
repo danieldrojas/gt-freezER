1dca030e2b91518e56a4fb0e6b47f3ef0b87a698
Michael Nava <M1cha3l.nava@gmail.com>
2020-06-20 17:39:40 -0400

    new changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var userLocation;
 $(document).ready(function () {
+  //grabs user location
   userLocation = navigator.geolocation.getCurrentPosition(
-    locationHandler,
+    locationHandler, // creates variable crd by calling getIceCreamStores function
     locationErrorHandler,
     options
   );
@@ -35,7 +36,7 @@ function locationHandler(pos) {
 function locationErrorHandler(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
-
+// this function retrieves the stores by searching for the term ice cream
 function getIceCreamStores(loc) {
   var data = { term: "ice cream" };
   if (loc && loc.latitude) {
@@ -50,13 +51,14 @@ function getIceCreamStores(loc) {
       return;
     }
   }
-  // console.log(loc);
+  // setting up variables for the yelp api AJAX call
   var latPointA = loc.latitude;
   var lonPointA = loc.longitude;
   var URL =
     "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?";
   var APIkey =
     "pZoeLz1SZU0FpO7ZzMtXIQ9dSW1UZ3Wp762C53LAb3zgJeMNvtwIQUCJL2-8hAAquHFK2XIiamEuOUXbuw5Rre3ie_pe1vknYXD9bCDCmd53ztY7KsdjUuIxlVvqXnYx";
+  // yelp ajax call
   $.ajax({
     url: URL,
     method: "GET",
@@ -68,47 +70,44 @@ function getIceCreamStores(loc) {
       // console.log(result);
     },
     error: function (error) {
-      console.log(error);
+      // console.log(error);
     },
   }).then(function (response) {
     console.log(response);
-    $("#iceCreamStores").empty();
+    // after receiving the data from yelp
+    $("#iceCreamStores").empty(); // empties the iceCreamStores div
+    //this add the image to the main col. In the event of click on the button
+    function addImage() {
+      var image = response.businesses[i].image_url;
+      console.log(image);
+
+      var imgDiv = $("<img>");
+      imgDiv.attr("src", image);
+      $("#icecream-img").append(imgDiv);
+    }
+    // runs 10 times and each time assigns the response to a variable
     for (var i = 0; i < 10; i++) {
       var iceCreamStores = response.businesses[i].name;
       var storeAddress = response.businesses[i].location.address1;
-      // console.log(storeAddress);
-      // console.log(iceCreamStores);
-      var iceCreamDistance = ("distance")
-      var timeToDistance = ("time")
+      var iceCreamDistance = "distance";
+      var timeToDistance = "time";
       var storeList = $("<button>").text(iceCreamStores);
       $(storeList).attr("class", "btn-block newIceCreamStoreButton");
       storeList.append($("<div>" + storeAddress + "</div>"));
       storeList.append($("<div>" + iceCreamDistance + "</div>"));
       storeList.append($("<div>" + timeToDistance + "</div>"));
-
       var listItem = $("<li>").append(storeList);
       $("#iceCreamStores").append(listItem);
       var latPointB = response.businesses[i].coordinates.latitude;
       var lonPointB = response.businesses[i].coordinates.longitude;
       var startingPos = latPointA + "," + lonPointA;
       var destinationPos = latPointB + "," + lonPointB;
-      console.log("Starting: ",startingPos);
-      console.log("Destination: ",destinationPos);
+      console.log("Starting: ", startingPos);
+      console.log("Destination: ", destinationPos);
       var mapQuestKey = "bDYO5JVsT0lGPolecMUk1lCGVNostBHT";
       var pointA = startingPos;
       var pointB = destinationPos;
-
-//change
-      //this add the image to the main col. In the event of click on the button
-      var image = response.businesses[i].image_url
-      console.log(image)
-
-      var imgDiv = $("<img>")
-      imgDiv.attr('src', image)
-      $("#icecream-img").append(imgDiv)
-
-
-
+      addImage(); //line 80
 
       var myURL =
         "https://www.mapquestapi.com/directions/v2/route?key=bDYO5JVsT0lGPolecMUk1lCGVNostBHT&from=" +
@@ -121,7 +120,6 @@ function getIceCreamStores(loc) {
         method: "GET",
       }).then(function (response) {
         console.log(response);
-
       });
     }
   });
